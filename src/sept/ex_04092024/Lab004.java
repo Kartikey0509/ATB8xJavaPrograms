a998f08d1306193082e2fed7ceab86aa50c85a0a
kartikey0509 <kartikey95@gmail.com>
2024-09-06 08:07:08 +0530

    Added a new file

--- src/sept/ex_04092024/Lab004.java
+++ src/sept/ex_04092024/Lab004.java
@@ -1,4 +1,15 @@
 package sept.ex_04092024;
 
 public class Lab004 {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+
+
+    }
 }
