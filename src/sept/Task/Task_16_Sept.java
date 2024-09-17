20124d0cf6c2fc4c591a6dd6d4e949a87a774940
kartikey0509 <kartikey95@gmail.com>
2024-09-17 23:11:09 +0530

    Added a new file

---
+++ src/sept/Task/Task_16_Sept.java
@@ -0,0 +1,21 @@
+package sept.Task;
+
+public class Task_16_Sept {
+    public static void main(String[] args) {
+        byte b = 10;
+        byte c = 10;
+        System.out.println(b+c);
+
+        short s = 10;
+
+        char d = 'A'; //65
+
+        int ss = s+d;
+
+        System.out.println(ss);
+
+        byte a = 10;
+        long l = 10l;
+        System.out.println(a+l);
+    }
+}
